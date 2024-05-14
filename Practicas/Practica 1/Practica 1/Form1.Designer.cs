f642a3c3eda412a0901eccbd7f10ab047063ba6b
Van Pratt González Ricardo Adolfo <147211017+VP-21212581@users.noreply.github.com>
2024-05-14 15:28:30 -0700

    Add files via upload

---
+++ Practicas/Practica 1/Practica 1/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Practica_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            cmbPuertos = new ComboBox();
+            lblTitulo = new Label();
+            lblPuerto = new Label();
+            btnConexion = new Button();
+            txtMensaje = new TextBox();
+            btnEnviar = new Button();
+            btnReiniciar = new Button();
+            SuspendLayout();
+            // 
+            // cmbPuertos
+            // 
+            cmbPuertos.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbPuertos.FormattingEnabled = true;
+            cmbPuertos.Items.AddRange(new object[] { "COM1", "COM2", "COM3", "COM4", "COM5", "COM5", "COM6", "COM7", "COM8", "COM9", "COM10", "COM11", "COM12", "COM13" });
+            cmbPuertos.Location = new Point(230, 134);
+            cmbPuertos.MaxLength = 10;
+            cmbPuertos.Name = "cmbPuertos";
+            cmbPuertos.Size = new Size(178, 28);
+            cmbPuertos.TabIndex = 0;
+            // 
+            // lblTitulo
+            // 
+            lblTitulo.AutoSize = true;
+            lblTitulo.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point);
+            lblTitulo.Location = new Point(98, 54);
+            lblTitulo.Name = "lblTitulo";
+            lblTitulo.Size = new Size(636, 54);
+            lblTitulo.TabIndex = 1;
+            lblTitulo.Text = "Demostración de conexión serial";
+            // 
+            // lblPuerto
+            // 
+            lblPuerto.AutoSize = true;
+            lblPuerto.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            lblPuerto.Location = new Point(98, 134);
+            lblPuerto.Name = "lblPuerto";
+            lblPuerto.Size = new Size(74, 28);
+            lblPuerto.TabIndex = 2;
+            lblPuerto.Text = "Puerto";
+            // 
+            // btnConexion
+            // 
+            btnConexion.Location = new Point(462, 134);
+            btnConexion.Name = "btnConexion";
+            btnConexion.Size = new Size(123, 29);
+            btnConexion.TabIndex = 3;
+            btnConexion.Text = "Conectarse";
+            btnConexion.UseVisualStyleBackColor = true;
+            btnConexion.Click += btnConexion_Click;
+            // 
+            // txtMensaje
+            // 
+            txtMensaje.Location = new Point(230, 229);
+            txtMensaje.Name = "txtMensaje";
+            txtMensaje.PlaceholderText = "Ingresa tu mensaje a enviar";
+            txtMensaje.Size = new Size(355, 27);
+            txtMensaje.TabIndex = 4;
+            txtMensaje.TextChanged += txtMensaje_TextChanged;
+            // 
+            // btnEnviar
+            // 
+            btnEnviar.Location = new Point(230, 287);
+            btnEnviar.Name = "btnEnviar";
+            btnEnviar.Size = new Size(123, 29);
+            btnEnviar.TabIndex = 5;
+            btnEnviar.Text = "Enviar mensaje";
+            btnEnviar.UseVisualStyleBackColor = true;
+            btnEnviar.Click += btnEnviar_Click;
+            // 
+            // btnReiniciar
+            // 
+            btnReiniciar.Location = new Point(462, 287);
+            btnReiniciar.Name = "btnReiniciar";
+            btnReiniciar.Size = new Size(123, 29);
+            btnReiniciar.TabIndex = 6;
+            btnReiniciar.Text = "Reiniciar";
+            btnReiniciar.UseVisualStyleBackColor = true;
+            btnReiniciar.Click += btnReiniciar_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnReiniciar);
+            Controls.Add(btnEnviar);
+            Controls.Add(txtMensaje);
+            Controls.Add(btnConexion);
+            Controls.Add(lblPuerto);
+            Controls.Add(lblTitulo);
+            Controls.Add(cmbPuertos);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox cmbPuertos;
+        private Label lblTitulo;
+        private Label lblPuerto;
+        private Button btnConexion;
+        private TextBox txtMensaje;
+        private Button btnEnviar;
+        private Button btnReiniciar;
+    }
+}
---
+++ Practicas/Practica 1/Practica 1/Form1.cs
@@ -0,0 +1,129 @@
+/*
+  Programa: Práctica 1: C# y Pico W
+  Autores: 
+    - López Machado Óscar Roberto - [Número de control]
+    - Morales Calvo Ángel Omar - [Número de control]
+    - Torres Equihua Victor Manuel - [Número de control]
+    - Van Pratt González Ricardo Adolfo - 21212581
+  Fecha: 29 de abril de 2024
+
+  Descripción:
+  Este programa se conecta a una Pico W mediante un programa de Windows Forms para que envíe un mensaje
+
+  Licencia: [Tipo de licencia]
+*/
+
+using System;
+using System.IO.Ports;
+using System.Threading;
+
+namespace Practica_1
+{
+    public partial class Form1 : Form
+    {
+        SerialPort puerto = new SerialPort();
+        bool check = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Llama un evento existente para no reescribir el código
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnReiniciar_Click(sender, e);
+        }
+
+        // Borra el mensaje escrito, reestablece el objeto seleccionado, y deshabilita el botón de enviar.
+        private void btnReiniciar_Click(object sender, EventArgs e)
+        {
+            txtMensaje.Clear();
+            cmbPuertos.SelectedIndex = 0;
+            btnEnviar.Enabled = false;
+        }
+
+        // Establece una conexión si se está desconectado, y lo desconecta si ya estaba conectado.
+        // Si el puerto es el apropiado, se conecta exitosamente.
+        // Si no swe puede, tira mensaje de error.
+        private void btnConexion_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!check)
+                {
+                    puerto.PortName = cmbPuertos.SelectedItem.ToString();
+                    puerto.BaudRate = 115200;
+                    puerto.DtrEnable = true;
+
+                    puerto.Open();
+
+                    MessageBox.Show($"Conectado al puerto {puerto.PortName}", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    cmbPuertos.Enabled = false;
+                    btnConexion.Text = "Desconectarse";
+                    check = true;
+                }
+                else
+                {
+                    puerto.Close();
+
+                    MessageBox.Show("Adiós", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    cmbPuertos.Enabled = true;
+                    btnEnviar.Enabled = false;
+                    btnConexion.Text = "Conectarse";
+                    check = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "¡Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        // Intenta hacer que la Raspberry escriba el mensaje enviado a una terminal.
+        // Si no hay un mensaje, despliega un mensaje indicando la falta de texto.
+        // Despliega un mensaje de error si no se completó la acción
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(txtMensaje.Text))
+                {
+                    puerto.WriteLine(txtMensaje.ToString());
+                    MessageBox.Show("Mensaje enviado", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("No hay mensaje para enviar", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "¡Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void txtMensaje_TextChanged(object sender, EventArgs e)
+        {
+            Actualizar();
+        }
+
+        // Verifica si la barra de texto con el mensaje está vacía o no.
+        // Si está vacía, deshabilita el botón para enviar el mensaje.
+        // Si no está vacía, lo habilita.
+        private void Actualizar()
+        {
+            if (string.IsNullOrEmpty(txtMensaje.Text) || check == false)
+            {
+                btnEnviar.Enabled = false;
+            }
+            else
+            {
+                btnEnviar.Enabled = true;
+            }
+        }
+    }
+}
